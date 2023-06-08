5d8f850a7a94785819f109df41c009fd1c5b30cd
David <david.izosimov@gmail.com>
2023-06-08 16:45:45 +0000

    Initializes repo.

---
+++ src/main/java/fr/inria/astor/core/solutionsearch/population/FitnessFunction.java
@@ -0,0 +1,22 @@
+package fr.inria.astor.core.solutionsearch.population;
+
+import fr.inria.astor.core.entities.ProgramVariant;
+import fr.inria.astor.core.entities.validation.VariantValidationResult;
+import fr.inria.astor.core.solutionsearch.extension.AstorExtensionPoint;
+
+/**
+ * Fitness function
+ * 
+ * @author Matias Martinez
+ *
+ */
+public interface FitnessFunction extends  AstorExtensionPoint {
+	
+	public double calculateFitnessValue(VariantValidationResult validationResult);
+
+	public double calculateFitnessValue(ProgramVariant variant, VariantValidationResult validationResult);
+
+	
+	public double getWorstMaxFitnessValue();
+	
+}
---
+++ src/main/java/fr/inria/astor/core/solutionsearch/population/TestCaseFitnessFunction.java
@@ -0,0 +1,50 @@
+package fr.inria.astor.core.solutionsearch.population;
+
+import fr.inria.astor.core.entities.validation.TestCaseVariantValidationResult;
+import fr.inria.astor.core.entities.validation.VariantValidationResult;
+import fr.inria.astor.core.entities.ProgramVariant;
+
+/**
+ * Fitness function based on test suite execution.
+ * 
+ * @author Matias Martinez
+ *
+ */
+public class TestCaseFitnessFunction implements FitnessFunction {
+	private static final double EDIT_PENALTY = 0.01;
+
+	/**
+	 * In this case the fitness value is associate to the failures: LESS FITNESS
+	 * is better.
+	 */
+	public double calculateFitnessValue(VariantValidationResult validationResult) {
+
+		if (validationResult == null)
+			return this.getWorstMaxFitnessValue();
+
+		TestCaseVariantValidationResult result = (TestCaseVariantValidationResult) validationResult;
+		return result.getFailureCount();
+	}
+
+	/**
+	 * In this case the fitness value is associated with the amount of edits to the
+	 * code that the solution required, the lower the score, the better.
+	 * 
+	 * @author David Izosimov & Mikael Esenbaev
+	 * 
+	 */
+	public double calculateFitnessValue(ProgramVariant variant, VariantValidationResult validationResult){
+			double fitness = calculateFitnessValue(validationResult);
+
+      int editCount = variant.getOperations().size();  // Assuming this method returns the number of edits
+      fitness += EDIT_PENALTY * editCount;
+
+      return fitness;
+	}
+
+	public double getWorstMaxFitnessValue() {
+
+		return Double.MAX_VALUE;
+	}
+
+}
